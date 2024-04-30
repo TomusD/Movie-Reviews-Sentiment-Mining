eebdb0af806f1d2d51370f630b35dabf7e2f8cab
Konstantinos Syrios <kostassir@yahoo.gr>
2024-04-30 13:52:39 +0300

    Updated the basic dataset preparation (structure, save as json)

--- dataset_management/dataset_management.py
+++ dataset_management/dataset_management.py
@@ -1,45 +1,61 @@
 import pandas as pd
-import ijson
 import json
+import uuid
 
 
 def prepare_keywords():
     dataframe = pd.read_excel("positive_negative_keywords.xlsx").drop(index=0)
     positive_df = dataframe["Positive Sense Word List"].dropna()
     negative_df = dataframe["Negative Sense Word List"].dropna()
-    return positive_df, negative_df
+    return positive_df.values.tolist(), negative_df.values.tolist()
 
 
 def rating_to_label(rating):
     rating = int(rating.split('/')[0])
     label = None
-    if rating <= 2:
-        label = "Very Negative"
-    elif rating <= 4:
+    if rating <= 4:
         label = "Negative"
     elif rating <= 6:
         label = "Neutral"
-    elif rating <= 8:
-        label = "Positive"
     elif rating <= 10:
-        label = "Very Positive"
+        label = "Positive"
     return label
 
 
-def prepare_basic_review_dataset():
+def prepare_basic_review_dataset(store_as_file=True):
     dataset_files = ["../crawler/data.json"]  # Add here all json files that contain movie reviews and their scores
-    data = []
+    data = {}
     for dataset_file_name in dataset_files:
+        # Will need method extraction for cleanup and implementation of other methods
+        # in case of adding data from different websites
+        # This handles iMDB data
         with open(dataset_file_name, 'r') as dataset_file:
             for line in dataset_file:
                 entry = json.loads(line)
                 if entry["rating"] != "None":
                     label = rating_to_label(entry["rating"])
-                    data.append([entry["review"], entry["rating"], label])
-        data = pd.DataFrame(data).dropna()
+                    data[str(uuid.uuid4())] = {"review": entry["review"], "rating": entry["rating"], "label": label}
+    if store_as_file:
+        with open("basic_dataset.json", 'w') as basic_dataset_file:
+            json.dump(data, basic_dataset_file)
     return data
 
 
+def count_keywords(positive_keywords, negative_keywords, basic_dataset):
+    positive_keyword_count = []
+    for keyword in positive_keywords:
+        positive_keyword_count.append([keyword, []])
+
+    negative_keyword_count = []
+    for keyword in negative_keywords:
+        negative_keyword_count.append([keyword, []])
+
+    for entry in basic_dataset:
+        # TODO find keywords in reviews and store their uid
+        return
+
+
 if __name__ == "__main__":
-    positive_keywords_df, negative_keywords_df = prepare_keywords()
-    prepare_basic_review_dataset()
+    # _positive_keywords, _negative_keywords = prepare_keywords()
+    _basic_dataset = prepare_basic_review_dataset()
+    # count_keywords(_positive_keywords, _negative_keywords, _basic_dataset)
