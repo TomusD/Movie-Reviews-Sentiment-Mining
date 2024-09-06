fa2df9fec0897835bfcb5505f6cf4a28b1c0c97d
Thanasis Triantafyllou <athantriantafyllou96@gmail.com>
2024-09-06 23:25:38 +0300

    Update validProxies.py

--- crawler/validProxies.py
+++ crawler/validProxies.py
@@ -6,5 +6,4 @@ with open ('valid_proxies.txt') as f:
     proxies = ips.append(proxies)
     #print(ips)
 
-# The code above is a snippet from validProxies.py. 
 # It reads the file valid_proxies.txt and stores the proxies in a list.
